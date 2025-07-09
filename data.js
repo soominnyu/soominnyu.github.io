1e45f0fc90857c97d857aeb09ebeebfabbde9491
김보현 <bhkim4550@pusan.ac.kr>
2025-07-09 20:05:57 +0900

    🎨 Add description

--- data.js
+++ data.js
@@ -1,42 +1,42 @@
 export const menuArray = [
     {
         name: "냉면",
-        ingredients: ["pepperoni", "mushrom", "mozarella"],
+        ingredients: ["시원한 냉면"],
         id: 0,
         price: 9000,
         emoji: "🧊"
     },
     {
         name: "밀면",
-        ingredients: ["beef", "cheese", "lettuce"],
+        ingredients: ["부드러운 밀면"],
         price: 8000,
         emoji: "🍜",
         id: 1
     },
         {
         name: "만두",
-        ingredients: ["grain, hops, yeast, water"],
+        ingredients: ["풍미있는 만두"],
         price: 6000,
         emoji: "🥟",
         id: 2
     },
         {
         name: "밀면사리",
-        ingredients: ["grain, hops, yeast, water"],
+        ingredients: ["추가 밀면 사리"],
         price: 2000,
         emoji: "2️⃣",
         id: 3
     },
         {
         name: "냉면사리",
-        ingredients: ["grain, hops, yeast, water"],
+        ingredients: ["추가 냉면 사리"],
         price: 3000,
         emoji: "3️⃣",
         id: 4
     },
         {
         name: "곱빼기",
-        ingredients: ["grain, hops, yeast, water"],
+        ingredients: ["곱빼기 추가"],
         price: 1000,
         emoji: "✖",
         id: 5
